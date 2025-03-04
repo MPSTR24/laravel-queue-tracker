e7255ec2a45d3cc9bcd2505ed74dd140e99dcb70
MPSTR24 <74561948+MPSTR24@users.noreply.github.com>
2025-03-04 22:08:09 +0000

    feat: basic queue tracker completed

--- src/Console/Commands/QueueTrackerCommand.php
+++ src/Console/Commands/QueueTrackerCommand.php
@@ -3,6 +3,7 @@
 namespace Mpstr24\QueueTracker\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
 
 class QueueTrackerCommand extends Command
 {
@@ -12,12 +13,23 @@ class QueueTrackerCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'queue:track {--queue=default}';
+    protected $signature = 'queue:track {--queue=default : This will be "default" unless specified}';
 
     protected $description = 'This command will display the current job in the selected queue.';
 
     public function handle(): int
     {
         $queue = $this->option('queue');
+        $cache_key = 'queue_tracker_'.$queue;
+
+        $current_job = Cache::get($cache_key);
+
+        if (!$current_job) {
+            $this->info('No job found in queue.');
+            return self::SUCCESS;
+        }
+
+        $this->info('Currently processing job: '.$current_job);
+        return self::SUCCESS;
     }
 }
--- src/QueueTrackerServiceProvider.php
+++ src/QueueTrackerServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace Mpstr24\QueueTracker;
 
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Mpstr24\QueueTracker\Console\Commands\QueueTrackerCommand;
 
@@ -25,5 +30,43 @@ class QueueTrackerServiceProvider extends ServiceProvider
                QueueTrackerCommand::class,
             ]);
         }
+
+        Event::listen(JobProcessing::class, function (JobProcessing $event) {
+            // when job starts processing add to cache so we can retrieve it in the command
+
+            // get queue
+            $queue = $event->job->getQueue() ?: 'default';
+
+            // make cache key
+            $cache_key = 'queue_tracker_' . $queue;
+            // using $queue so the user can pick
+
+            try{
+                $job_name = $event->job->resolveName();
+            }catch (\Exception $e){
+                $job_name = get_class($event->job);
+            }
+
+            // add that job name to cache for 30 minutes in case the job is huge
+            Cache::put($cache_key, $job_name, now()->addMinutes(30));
+
+        });
+
+        Event::listen(JobProcessed::class, function (JobProcessed $event) {
+            // clear that queue's cache
+            $this->clearCache($event);
+        });
+
+        Event::listen(JobFailed::class, function (JobFailed $event) {
+            // clear that queue's cache
+            $this->clearCache($event);
+        });
+    }
+
+    private function clearCache($event): void
+    {
+        $queue = $event->job->getQueue() ?: 'default';
+        $cache_key = 'queue_tracker_' . $queue;
+        Cache::forget($cache_key);
     }
 }
